ee1d6a5d70dd0d9d04f27d5cd5ab1d77249000a2
Rocco Himel <rockyrbn1847@gmail.com>
2025-12-16 07:22:22 -0500

    Fixed control bug
    Last program wouldn't let me shoot unless a key was constantly pressed. I fixed that and also removed screen buffer.

--- terminvaders.c
+++ terminvaders.c
@@ -1,10 +1,14 @@
+// terminvaders.c
+
 #include <stdio.h>
-#include <time.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <termios.h>
+#include <fcntl.h>
 
+
+// check for keys pressed insted of input
 struct termios original;
 
 void enableRawMode() {
@@ -15,50 +19,74 @@ void enableRawMode() {
         tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
+// disable the function
 void disableRawMode() {
         tcsetattr(STDIN_FILENO, TCSAFLUSH, &original);
 }
 
+// plot a string
 void plot(int x, int y, char *z) {
+        if (x < 1) x = 1;
+        if (y < 1) y = 1;
         printf("\033[%d;%dH%s", y, x, z);
 }
 
+// main
 int main() {
+        // clear buffer and enable alt term
+        setvbuf(stdout, NULL, _IONBF, 0);
         system("tput smcup; clear");
         printf("\033[?25l");
 
+        // enable raw keys
         enableRawMode();
+        fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
 
+        // get max X and Y
         struct winsize w;
         ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-        int maxScreenX = w.ws_col - 1;
-        int maxScreenY = w.ws_row - 1;
+        int maxScreenX = w.ws_col;
+        int maxScreenY = w.ws_row;
 
+        // initialize tank positions
         int tankPositionX = (maxScreenX / 2) - 1;
         int tankPositionY = maxScreenY - 3;
         char *tankChar = "<|>";
 
+        // initialize bullet positions
         int bulletPositionX = 0;
         int bulletPositionY = 0;
-        char *bulletChar = ".";
+        char *bulletChar = "o";
         int bulletActive = 0;
 
+        // game loop
         while (1) {
                 printf("\033[H\033[J");
 
+                // set bounderies for tank
+                if (tankPositionX < 1) tankPositionX = 1;
+                if (tankPositionX > maxScreenX - 2)
+                        tankPositionX = maxScreenX - 2;
+
+                // plot characters
                 plot(tankPositionX, tankPositionY, tankChar);
 
+                // if bulletActive, y--
                 if (bulletActive) {
-                        plot(bulletPositionX, bulletPositionY, bulletChar);
                         bulletPositionY--;
 
                         if (bulletPositionY <= 1) {
                                 bulletActive = 0;
+                        } else {
+                                plot(bulletPositionX, bulletPositionY, bulletChar);
                         }
                 }
 
-                char input = getchar();
+                // get inputs
+                char input = 0;
+                read(STDIN_FILENO, &input, 1);
 
+                // controls
                 if (input == 'a') tankPositionX--;
                 if (input == 'd') tankPositionX++;
 
@@ -69,8 +97,10 @@ int main() {
                 }
 
                 if (input == 'q') break;
+                usleep(10000);
         }
 
+        // end game
         disableRawMode();
         printf("\033[?25h");
         system("tput rmcup");
